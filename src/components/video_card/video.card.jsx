9c58eeeb96160d24e62e1267eb8c89f4095ea973
Jack <raxmatoveldor37@gmail.com>
2025-08-03 08:24:22 +0500

    all right and worked well

--- src/components/video_card/video.card.jsx
+++ src/components/video_card/video.card.jsx
@@ -1,28 +1,34 @@
 import {Fragment} from 'react';
+import {Link} from 'react-router-dom';
+import dayjs from 'dayjs';
+import './video.card.css';
 import {Avatar, Card, CardContent, CardMedia, Stack, Typography} from '@mui/material';
 import {CheckCircle} from '@mui/icons-material';
-import dayjs from 'dayjs';
 
 import {colors} from '../../constants/colors';
 
 const VideoCard = ({video}) => {
     return (
         <Card sx={{width: {xs: '100%', sm: '360px', md: '320px'}, boxShadow: 'none', borderRadius: 0}}>
-            <CardMedia
-                image={video?.snippet?.thumbnails?.high?.url}
-                alt={video?.snippet?.title}
-                sx={{width: {xs: '100%', sm: '360px', md: '320px'}, height: '180px'}}
-            />
+            <Link to={`/video/${video.id.videoId}`}>
+                <CardMedia
+                    image={video?.snippet?.thumbnails?.high?.url}
+                    alt={video?.snippet?.title}
+                    sx={{width: {xs: '100%', sm: '360px', md: '320px'}, height: '180px'}}
+                />
+            </Link>
             <CardContent sx={{background: colors.primary, height: '200px', position: 'relative'}}>
-                <Typography my={5} sx={{opacity: '.4'}}>
-                    {dayjs(video?.snippet?.publishedAt).format('YYYY-MM-DD')}
-                </Typography>
-                <Typography variant="subtitle1" fontWeight={'bold'}>
-                    {video?.snippet?.title.slice(0, 35)}...
-                </Typography>
-                <Typography variant="subtitle2" sx={{opacity: '.6'}}>
-                    {video?.snippet?.description.slice(0, 70)}
-                </Typography>
+                <Link to={`/video/${video.id.videoId}`}>
+                    <Typography my={5} sx={{opacity: '.4'}}>
+                        {dayjs(video?.snippet?.publishedAt).format('YYYY-MM-DD')}
+                    </Typography>
+                    <Typography variant="subtitle1" fontWeight={'bold'}>
+                        {video?.snippet?.title.slice(0, 35)}...
+                    </Typography>
+                    <Typography variant="subtitle2" sx={{opacity: '.6'}}>
+                        {video?.snippet?.description.slice(0, 70)}
+                    </Typography>
+                </Link>
                 <Fragment>
                     <Stack direction={'row'}
                            position={'absolute'}
--- src/components/video_detail/video.detail.jsx
+++ src/components/video_detail/video.detail.jsx
@@ -1,6 +1,47 @@
+import {useParams} from 'react-router-dom';
+import {useEffect, useState} from 'react';
+import ReactPlayer from 'react-player';
+import YouTube from 'react-youtube';
+import {ApiService} from '../../service/api.service';
+import {Box} from '@mui/material';
+import './video.detail.css';
+
 const VideoDetail = () => {
+    const {id} = useParams();
+    const [videoDetail, setVideoDetail] = useState(null);
+
+    useEffect(() => {
+        try {
+            const getData = async () => {
+                const data = await ApiService.fetching(`videos?part=snippet,statistics&id=${id}`);
+                console.log('data =>', data);
+                setVideoDetail(data.items[0]);
+            }
+            getData();
+        } catch (e) {
+            console.log(e);
+        }
+    }, [id]);
+
+    const {
+        snippet: {title, channelId, channelTitle, description, tags, thumbnails},
+        statistics: {viewCount, likeCount, commentCount}
+    } = videoDetail || {snippet: {}, statistics: {}};
+
     return (
-        <div>VideoDetail</div>
+        <Box minHeight={'90vh'} mb={10}>
+            <Box display={'flex'}>
+                <Box width={'75%'} mx={2} className={'react-player'}>
+                    {/*<ReactPlayer*/}
+                    {/*    url="https://www.youtube.com/watch?v=PT6cKHsMzSQ"*/}
+                    {/*    className={'react-player'}*/}
+                    {/*    controls*/}
+                    {/*/>*/}
+                    <YouTube videoId="PT6cKHsMzSQ" opts={{width: '100%', height: '800hv'}}/>
+                </Box>
+                <Box width={'25%'}>Suggested videos</Box>
+            </Box>
+        </Box>
     )
 }
 
